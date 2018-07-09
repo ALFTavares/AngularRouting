24628b41f0bc9c7fcfa66703485500edc12bc8fc
mouse <alftavares1985@gmail.com>
2018-07-09 11:57:42 +0100

    passing optional params

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 import { IProduct } from './product';
 import { ProductService } from './product.service';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
     templateUrl: './app/products/product-detail.component.html'
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
     pageTitle: string = 'Product Detail';
     product: IProduct;
     errorMessage: string;
 
-    constructor(private productService: ProductService) { }
+    constructor(private productService: ProductService,
+                private route: ActivatedRoute) {
+    }
+
+    ngOnInit(): void {
+        this.getProduct(+this.route.snapshot.params['id']);
+    }
 
     getProduct(id: number) {
         this.productService.getProduct(id).subscribe(
